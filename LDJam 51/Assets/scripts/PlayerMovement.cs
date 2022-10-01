68702870ae576ff1ae2869205a6f67ae24f5cd6c
TriDivide <liamsmitha@gmail.com>
2022-10-01 11:11:29 +0100

    - added player movement.

--- LDJam 51/Assets/scripts/PlayerMovement.cs
+++ LDJam 51/Assets/scripts/PlayerMovement.cs
@@ -1,6 +1,34 @@
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
-    
-    private Rigidbody2D body
+
+     private Rigidbody2D body;
+    [SerializeField] private float speed = 20.0f;
+    [SerializeField] private float jumpSpeed = 10.0f;
+    private bool isGrounded;
+    private void Awake() {
+        body = GetComponent<Rigidbody2D>();
+
+
+    }
+
+    private void Update() {
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);    
+
+        if (Input.GetKey(KeyCode.Space) && isGrounded) {
+            Jump();
+        }
+    }
+
+    private void Jump() {
+        body.velocity = new Vector2(body.velocity.x, jumpSpeed);
+        isGrounded = false;
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if(collision.gameObject.tag == "Ground") {
+            isGrounded = true;
+        }
+    }
 }

77c2fc517f2bae7dcfe52274bce8468392b007fb
TriDivide <liamsmitha@gmail.com>
2022-10-02 23:26:47 +0100

    - AI is now working

--- LDJam 51/Assets/scripts/EnemyAI.cs
+++ LDJam 51/Assets/scripts/EnemyAI.cs
@@ -3,7 +3,7 @@ using Pathfinding;
 
 public class EnemyAI : MonoBehaviour {
 
-    [SerializeField] private Transform target;
+    
     [SerializeField] private float speed = 200f;
     public float nextWaypointDistance = 3f;
 
@@ -14,18 +14,52 @@ public class EnemyAI : MonoBehaviour {
     private Seeker seeker;
     private Rigidbody2D rb;
 
+    private Transform target;
 
     private void Start() {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
         seeker = GetComponent<Seeker>();
         rb = GetComponent<Rigidbody2D>();
 
-        seeker.StartPath(rb.position, target.position, OnPathComplete);
+        InvokeRepeating("UpdatePath", 0f, .5f);
     }
 
     private void OnPathComplete(Path p) {
         if (!p.error) {
             path = p;
             currentWaypoint = 0;
+        }
+    }
+
+    private void UpdatePath() {
+        if (seeker.IsDone()) {
+            seeker.StartPath(rb.position, target.position, OnPathComplete);
+        }
+    }
+
+    private void FixedUpdate() {
+        if (path == null) {
+            return;
+        }
+
+        if (currentWaypoint >= path.vectorPath.Count) {
+            reachedEndOfPath = true;
+            return;
+        }
+        else {
+            reachedEndOfPath = false;
+        }
+
+        Vector2 direction = ((Vector2) path.vectorPath[currentWaypoint] - rb.position).normalized;
+
+        Vector2 force = direction * speed * Time.deltaTime;
+
+        rb.AddForce(force);
+
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+
+        if (distance < nextWaypointDistance) {
+            currentWaypoint++;
         }
     }
 }

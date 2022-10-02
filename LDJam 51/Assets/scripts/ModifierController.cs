12acbbb9b84178c7aee435a95d1cdca73b909798
TriDivide <liamsmitha@gmail.com>
2022-10-02 15:29:44 +0100

    - finished work on the mechanics of the modifier system.

--- LDJam 51/Assets/scripts/ModifierController.cs
+++ LDJam 51/Assets/scripts/ModifierController.cs
@@ -4,10 +4,35 @@ public class ModifierController : MonoBehaviour {
 
     private bool playerOnPad = false;
 
-    private void FixedUpdate() {
+    private enum Modifier { speed, increasedPlayerDamage, }
+
+    private Modifier currentModifier;
+
+
+    private void Start() {
+        InvokeRepeating("UpdateCurrentModifier", .0001f, 10f);
+    }
+
+    private void UpdateCurrentModifier() {
+        currentModifier = (Modifier)Random.Range(0, 2);
+        print(currentModifier);
+    }
+
+    private void Update() {
         if (playerOnPad && Input.GetKeyDown(KeyCode.P)) {
-            ModifierManager.Instance.DoublePlayerSpeed();
-            print("upgrade");
+            print(currentModifier);
+            print("trying to change");
+            switch(currentModifier) {
+                case Modifier.speed:
+                    ModifierManager.Instance.DoublePlayerSpeed();
+                    print("upgrade");
+                    break;
+                case Modifier.increasedPlayerDamage:
+                    ModifierManager.Instance.DoubleBulletDamage();
+                    print("upgrade");
+                    break;
+            }
+
         }        
     }
 
--- LDJam 51/Assets/scripts/ModifierManager.cs
+++ LDJam 51/Assets/scripts/ModifierManager.cs
@@ -11,6 +11,10 @@ public class ModifierManager : MonoBehaviour {
 
     public float bulletDamage = 10f;
     [SerializeField] private float defaultBulletDamage = 10f;
+
+
+    public bool hasModifierSet { get; private set; } = false;
+
     private void Awake() {
         // If there is an instance, and it's not me, delete myself.
 
@@ -23,17 +27,33 @@ public class ModifierManager : MonoBehaviour {
     }
 
     public void DoublePlayerSpeed() {
-        playerMovementSpeed = defaultPlayerMovementSpeed * 2f;
-        Invoke("ResetPlayerSpeed", 10f); //reset the playerspeed after 10 seconds.
+        if (!hasModifierSet) {
+            print("Doubled player speed");
+            playerMovementSpeed = defaultPlayerMovementSpeed * 2f;
+            hasModifierSet = true;
+            Invoke("ResetPlayerSpeed", 10f); //reset the playerspeed after 10 seconds.
+        }
     }
 
     private void ResetPlayerSpeed() {
+        hasModifierSet = false;
         print("resetting the player speed");
         playerMovementSpeed = defaultPlayerMovementSpeed;
     }
 
-    
+    public void DoubleBulletDamage() {
+        if (!hasModifierSet) {
+            print("doubled bullet damage");
+            hasModifierSet = true;
+            bulletDamage = defaultBulletDamage * 2f;
+            Invoke("ResetBulletDamage", 10f);
+        }
+    }
 
-
+    private void ResetBulletDamage() {
+        hasModifierSet = false;
+        print("resetting the bullet damage");
+        bulletDamage = defaultBulletDamage;
+    }
 }
 
